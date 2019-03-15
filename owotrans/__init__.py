491c08572e4d126dca5796d1f589189095b13da4
usvimal <usvimal@gmail.com>
2019-03-15 23:32:06 +0800

    prepare to go live

---
+++ owotrans/__init__.py
@@ -0,0 +1,1 @@
+from owotrans import owo
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="owotranslator",
+    version="0.0.1",
+    author="Vimal",
+    author_email="usvimal@gmail.com",
+    description="A package that owoifies your text",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/usvimal/owotranslator",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
