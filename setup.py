3da911acfab854693c9ca3f0e0954d8de1a7352c
usvimal <usvimal@gmail.com>
2019-08-15 23:37:02 +0800

    Revert "push version 0.03"
    This reverts commit a0a6f2b32f0287157783fc605f8c21e865f9b7ae.

--- setup.py
+++ setup.py
@@ -5,17 +5,15 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="owotranslator",
-    version="0.0.3",
+    version="0.0.2",
     author="Vimal",
     author_email="usvimal@gmail.com",
     description="A package that owoifies your text",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/usvimal/owotranslator",
-    download_url = 'https://github.com/usvimal/owotrans/archive/0.0.3.tar.gz',
+    download_url = 'https://github.com/usvimal/owotrans/archive/0.0.2.tar.gz',
     packages=setuptools.find_packages(),
-    use_scm_version={'write_to': '%s/_version.py' % 'owotrans'},
-    setup_requires=['setuptools_scm'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
