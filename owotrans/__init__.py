a1ef560cc3a667b4c5e556d1ff6866ddf3831876
usvimal <usvimal@gmail.com>
2019-08-15 23:59:24 +0800

    update to v0.0.5
    fix the default text

--- owotrans/__init__.py
+++ owotrans/__init__.py
@@ -3,11 +3,11 @@ from pkg_resources import get_distribution, DistributionNotFound
 import os.path
 
 try:
-    _dist = get_distribution('foobar')
+    _dist = get_distribution('owotranslator')
     # Normalize case for Windows systems
     dist_loc = os.path.normcase(_dist.location)
     here = os.path.normcase(__file__)
-    if not here.startswith(os.path.join(dist_loc, 'foobar')):
+    if not here.startswith(os.path.join(dist_loc, 'owotranslator')):
         # not installed, but there is another version that *is*
         raise DistributionNotFound
 except DistributionNotFound:
